b295aadda67052dcc336a4571c96df46506969a6
rofe <github@rofe.com>
2023-02-03 11:31:19 +0100

    chore: svg alt text support

--- blocks/svg/svg.js
+++ blocks/svg/svg.js
@@ -1,9 +1,14 @@
 export default function decorate(block) {
-  const url = block.textContent.trim();
-  block.innerHTML = '';
+  const link = block.querySelector('a');
+  if (!link) return;
+  const url = link.getAttribute('href');
   if (url) {
+    link.remove();
+    const altText = block.textContent.trim();
+    block.innerHTML = '';
     const img = document.createElement('img');
     img.src = url;
+    img.alt = altText;
     block.append(img);
   }
 }
