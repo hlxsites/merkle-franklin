8366016b98f756c9c345073e74c43cc6dde39133
rsingh34 <rituraj.singh@emea.merkleinc.com>
2023-02-22 09:50:40 +0100

    feat: Added hash link class for toc and hover for cards

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -3,6 +3,11 @@ export default function decorate(block) {
   const cols = [...block.lastElementChild.children];
   cols.forEach((col, i) => {
     col.classList.add('toc-item', `toc-${i + 1}`);
+    col.querySelectorAll('a').forEach((a)=>{
+      if(a.getAttribute("href").charAt(0) === '#'){
+        a.classList.add("hash-link");
+      }
+    })
   });
   block.classList.add(`toc-${cols.length}-cols`);
 }
