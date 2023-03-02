4897f600de2e07337002222bc0e1b16771c79454
arpanmerkle <arpan.garg@merkle.com>
2023-03-02 16:01:05 +0100

    feat: Adding eslint rule change

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     // allow reassigning param
     'no-param-reassign': [2, { props: false }],
-    'custom-property-no-missing-var-function': [0, { ignoreProperties: ['transition', 'transition-property'] }],
+    'custom-property-no-missing-var-function': [false],
     'linebreak-style': ['error', 'unix'],
     'import/extensions': ['error', {
       js: 'always',
